e903c6e548ec999a5b3cb47fde5990de2969b0dd
Kenpachi-Zaraki-art <top4ikrudua@gmail.com>
2024-09-24 09:02:41 +0300

    LB3

--- LR3/file.cpp
+++ LR3/file.cpp
@@ -26,3 +26,62 @@ int main() {
     return 0;
 }
 //Task2
+//N21
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char riverInitial;
+    char choice;
+    do {
+    cout << "Available rivers to choose from:\n";
+    cout << "D - Dnipro\n";
+    cout << "A - Amazon\n";
+    cout << "N - Nile\n";
+    cout << "V - Volga\n";
+    cout << "M - Mississippi\n";
+    cout << "Enter the first letter of the river's name: "; 
+    cin >> riverInitial;
+
+   
+    riverInitial = toupper(riverInitial);
+
+    switch (riverInitial) {
+        case 'D': // Example for "Dnipro"
+            cout << "River: Dnipro\n";
+            cout << "Length: 2,201 km\n";
+            cout << "Number of tributaries: ~13 major tributaries\n";
+            break;
+        case 'A': // Example for "Amazon"
+            cout << "River: Amazon\n";
+            cout << "Length: 7,000 km\n";
+            cout << "Number of tributaries: Over 1,100\n";
+            break;
+        case 'N': // Example for "Nile"
+            cout << "River: Nile\n";
+            cout << "Length: 6,650 km\n";
+            cout << "Number of tributaries: 2 major tributaries (White Nile and Blue Nile)\n";
+            break;
+        case 'V': // Example for "Volga"
+            cout << "River: Volga\n";
+            cout << "Length: 3,530 km\n";
+            cout << "Number of tributaries: 200+ major tributaries\n";
+            break;
+        case 'M': // Example for "Mississippi"
+            cout << "River: Mississippi\n";
+            cout << "Length: 3,766 km\n";
+            cout << "Number of tributaries: ~250\n";
+            break;
+        default:
+            cout << "Information about the river starting with this letter is not available.\n";
+    }
+    cout << "\nDo you want to choose another river? (y = continue, x = exit): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y'); 
+
+    cout << "Program finished.\n";
+
+    return 0;
+}
